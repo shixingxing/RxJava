78094ed41a7e931c24d9322023ddb025ca8ea900
shixingxing <shixingxing@laoyuegou.com>
2019-03-08 11:19:35 +0800

    update

--- app/src/main/java/com/test/rxjava/model/AppInfo.java
+++ app/src/main/java/com/test/rxjava/model/AppInfo.java
@@ -1,12 +1,34 @@
 package com.test.rxjava.model;
 
-import lombok.Data;
 
-@Data
 public class AppInfo implements Comparable<Object> {
     private long lastUpdateTime;
     private String name;
     private String icon;
+
+    public long getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(long lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
 
     public AppInfo() {
     }
