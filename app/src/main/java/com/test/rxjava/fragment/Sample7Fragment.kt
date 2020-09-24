63fb2399758e8039b8ce1cfbaf88a03cd8c5c8f0
shixingxing <370189035@qq.com>
2020-09-24 11:20:49 +0800

    指纹识别

--- app/src/main/java/com/test/rxjava/fragment/Sample7Fragment.kt
+++ app/src/main/java/com/test/rxjava/fragment/Sample7Fragment.kt
@@ -4,12 +4,69 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.test.rxjava.BaseFragment
 import com.test.rxjava.R
+import kotlinx.android.synthetic.main.fragment_sample7.*
+import java.util.concurrent.Executor
 
 class Sample7Fragment : BaseFragment() {
 
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        executor = ContextCompat.getMainExecutor(activity)
+        biometricPrompt = BiometricPrompt(this, executor,
+                object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(errorCode: Int,
+                                                       errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        Toast.makeText(activity,
+                                "Authentication error: $errString", Toast.LENGTH_SHORT)
+                                .show()
+                    }
+
+                    override fun onAuthenticationSucceeded(
+                            result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        Toast.makeText(activity,
+                                "Authentication succeeded!", Toast.LENGTH_SHORT)
+                                .show()
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        Toast.makeText(activity, "Authentication failed",
+                                Toast.LENGTH_SHORT)
+                                .show()
+                    }
+                })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Use account password")
+                .build()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_sample7, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        login.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        biometricPrompt.authenticate(promptInfo)
     }
 }
