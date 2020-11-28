9acc5d8a861a0dbbecc6d0b6992b902333a113cc
shixingxing <370189035@qq.com>
2020-11-28 15:11:10 +0800

    qr code

--- app/src/main/java/com/test/rxjava/fragment/MainFragment.kt
+++ app/src/main/java/com/test/rxjava/fragment/MainFragment.kt
@@ -1,10 +1,13 @@
 package com.test.rxjava.fragment
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.PermissionChecker
 import androidx.navigation.Navigation
 import com.test.rxjava.BaseFragment
 import com.test.rxjava.CaptureActivity
@@ -12,6 +15,10 @@ import com.test.rxjava.R
 import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : BaseFragment() {
+
+    companion object {
+        const val REQUEST_CAMERA = 1000
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
@@ -22,7 +29,14 @@ class MainFragment : BaseFragment() {
 
         sample1.setOnClickListener { v ->
 //            Navigation.findNavController(v).navigate(MainFragmentDirections.sample1Action())
-            startActivity(Intent(context, CaptureActivity::class.java))
+            context?.let {
+                if (PermissionChecker.checkSelfPermission(it, Manifest.permission.CAMERA) != PermissionChecker.PERMISSION_GRANTED) {
+                    requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA)
+                } else {
+                    startActivity(Intent(it, CaptureActivity::class.java))
+                }
+            }
+
         }
         sample2.setOnClickListener { v ->
             Navigation.findNavController(v).navigate(MainFragmentDirections.sample2Action())
@@ -47,6 +61,19 @@ class MainFragment : BaseFragment() {
         }
         sample9.setOnClickListener { v ->
             Navigation.findNavController(v).navigate(MainFragmentDirections.sample9Action())
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CAMERA) {
+            for (result in grantResults) {
+                if (result != PermissionChecker.PERMISSION_GRANTED) {
+                    Toast.makeText(context, "Permission Error", Toast.LENGTH_LONG).show()
+                    return
+                }
+            }
+            startActivity(Intent(context, CaptureActivity::class.java))
         }
     }
 }
