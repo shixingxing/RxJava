102a16f38c0765ac9ad92dca2261c912249cdc44
shixingxing <370189035@qq.com>
2020-11-30 09:57:26 +0800

    添加重新开始扫描任务

--- app/src/main/java/com/test/rxjava/CaptureActivity.kt
+++ app/src/main/java/com/test/rxjava/CaptureActivity.kt
@@ -1,6 +1,7 @@
 package com.test.rxjava
 
 import android.graphics.Bitmap
+import android.widget.Toast
 import com.google.zxing.Result
 import com.google.zxing.client.android.CaptureActivity
 
@@ -8,6 +9,9 @@ class CaptureActivity : CaptureActivity() {
 
     override fun handleDecode(rawResult: Result?, barcode: Bitmap?, scaleFactor: Float) {
         super.handleDecode(rawResult, barcode, scaleFactor)
+        Toast.makeText(this, rawResult?.text, Toast.LENGTH_LONG).show()
+        //500毫秒后重新开始扫描
+        restartPreviewAfterDelay(500)
     }
 
 }
